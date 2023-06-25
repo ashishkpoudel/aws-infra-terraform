7db9729c3e194a8256e9f4855b7f8760e2cd3f37
Ashish K. Poudel <routeasis@gmail.com>
2023-06-25 15:20:38 +0545

    sample tf config

--- cicd/terraform/main.tf
+++ cicd/terraform/main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "akp-terraform-state"
+    key = "api"
+    region = "us-east-1"
+  }
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
